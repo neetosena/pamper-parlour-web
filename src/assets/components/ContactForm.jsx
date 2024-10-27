cc38d15e775ca3881f2eeb6495f2946a2c35b5d0
Neto Sena <neetosena@gmail.com>
2024-10-27 19:35:57 +0000

    contact form

--- src/assets/components/ContactForm.jsx
+++ src/assets/components/ContactForm.jsx
@@ -33,7 +33,7 @@ const ContactForm = () => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...values }),
+      body: encode({ "form-name": "pamper-parlour-contact", ...values }),
     })
       .then(() => {
         console.log("Form sucessfully submitted");
@@ -78,7 +78,11 @@ const ContactForm = () => {
             data-netlify="true"
             netlify-honeypot="bot-field"
           >
-            <input type="hidden" name="form-name" value="contact" />
+            <input
+              type="hidden"
+              name="form-name"
+              value="pamper-parlour-contact"
+            />
             <input type="hidden" name="bot-field" />
             <div>
               <Field
