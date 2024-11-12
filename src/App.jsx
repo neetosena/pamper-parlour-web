efa4a88feeff82f3c9ddbb420daa0d5b3098173d
Neto Sena <neetosena@gmail.com>
2024-11-12 21:13:06 +0000

    test component deleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./assets/pages/Home";
 
 import "./App.css";
-import Test from "./assets/pages/Test";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
